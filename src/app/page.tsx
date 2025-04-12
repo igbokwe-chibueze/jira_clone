49703a96c37844b99168b9ab6458eb8302155b02
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-04-12 08:57:55 +0100

    Installed Shadcn.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className=' homeStyle'>page</div>
+    <div className=' '>
+      <span className='text-3xl font-bold'>This is the main page</span>
+      
+      <Button
+        variant={'ghost'}
+        size={'lg'}
+      >
+        Click me
+      </Button>
+    </div>
   )
 }
 
