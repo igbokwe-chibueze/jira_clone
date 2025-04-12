b9e183d807123d3a2a99da8d8ae780b65d5aa179
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-04-12 10:07:37 +0100

    Installed multiple components and costumized button, input and select components.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,68 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className=' '>
+    <div className='text-center'>
       <span className='text-3xl font-bold'>This is the main page</span>
+
+      <div>
+      <Input/>
+      </div>
+
+      <div className=''>
+        <Button
+        >
+          Primary
+        </Button>
+
+        <Button
+          variant={'destructive'}
+        >
+          Destructive
+        </Button>
+
+        <Button
+          variant={'ghost'}
+        >
+          Ghost
+        </Button>
+
+        <Button
+          variant={'link'}
+        >
+          Link
+        </Button>
+
+        <Button
+          variant={'outline'}
+        >
+          Outline
+        </Button>
+
+        <Button
+          variant={'secondary'}
+          size={'lg'}
+        >
+          Secondary
+        </Button>
+
+        <Button
+          variant={'muted'}
+        >
+          Muted
+        </Button>
+
+        <Button
+          variant={'teritary'}
+        >
+          Teritary
+        </Button>
+
+
+      </div>
       
-      <Button
-        variant={'ghost'}
-        size={'lg'}
-      >
-        Click me
-      </Button>
     </div>
   )
 }
