0a7ed458577ef1c6a0157d7e7d8740a22b8d1e6b
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-04-13 06:32:19 +0100

    Setting Up Hono.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,23 @@
+//app/api/[[...route]]/route.ts
+
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
+
+app.get("/project/:projectId", (c) => {
+    const projectId = c.req.param("projectId")
+    console.log(projectId)
+    return c.json({project: projectId})
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
