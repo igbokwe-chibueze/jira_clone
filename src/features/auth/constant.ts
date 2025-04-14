4f9657c33c5f02923d53aafdb3ff7b73cfa12f42
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-04-14 23:35:42 +0100

    Setup Appwrite
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

---
+++ src/features/auth/constant.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE="igb-render-board-session";
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -3,28 +3,67 @@
 import { Hono } from 'hono';
 import {zValidator} from "@hono/zod-validator";
 import { signInSchema, signUpSchema } from '../schemas';
+import { createAdminClient } from '@/lib/appwrite';
+import { deleteCookie, setCookie } from 'hono/cookie';
+import { AUTH_COOKIE } from '../constant';
+import { ID } from 'node-appwrite';
 
 
 const app = new Hono()
     .post(
         "/signIn",
         zValidator("json", signInSchema),
-        (c) => {
+        async(c) => {
             const { email, password } = c.req.valid("json");
-            console.log(email, password);
-            return c.json(c.req.valid("json"));
+            
+            const { account } = await createAdminClient();
+            const session = await account.createEmailPasswordSession(email, password);
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            })
+
+            return c.json({success: true});
         }
     )
     .post(
         "/signUp",
         zValidator("json", signUpSchema),
-        (c) => {
+        async (c) => {
             const { name, email, password } = c.req.valid("json");
-            console.log(name, email, password);
-            return c.json(c.req.valid("json"));
-        }
-    );
-    
 
+            const { account } = await createAdminClient();
+            await account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+            const session = await account.createEmailPasswordSession(email, password);
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            })
+
+            return c.json({success: true});
+        }
+    )
+    .post(
+        "/signOut",
+        (c) => {
+            deleteCookie(c, AUTH_COOKIE);
+
+            return c.json({success: true});
+        }
+    )
 
 export default app;
